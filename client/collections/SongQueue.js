9b30d607589fad8cc45667fc7dea2eab47f24612
Justin Mendelson <jmendelson22@gmail.com>
2017-01-08 19:34:22 -0800

    Dequeue capability

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,15 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if(this.length === 1) {
+        this.playFirst();
+      }
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
--- test/spec/SongQueueSpec.js
+++ test/spec/SongQueueSpec.js
@@ -20,13 +20,13 @@ describe('SongQueue', function() {
   });
 
   describe('when a song is added', function() {
-    xit('plays it if it is the only song in the song queue', function() {
+    it('plays it if it is the only song in the song queue', function() {
       var songQueue = new SongQueue();
       songQueue.add(songData1);
       expect(playSpy).to.have.been.called;
     });
 
-    xit('does nothing if it is not the only song in the song queue', function() {
+    it('does nothing if it is not the only song in the song queue', function() {
       var songQueue = new SongQueue(songData1);
       songQueue.add(songData2);
       expect(playSpy).to.have.not.been.called;
@@ -67,7 +67,7 @@ describe('SongQueue', function() {
   });
 
   describe('playFirst', function() {
-    xit('plays the first song in the queue', function() {
+    it('plays the first song in the queue', function() {
       sinon.spy(SongModel.prototype, 'play');
       var songQueue = new SongQueue(songData1);
       songQueue.playFirst();
